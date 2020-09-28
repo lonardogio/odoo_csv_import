2552db6712be1782df8cceae85a9eb28dd22d60c
gioacchino.lonardo <gioacchino.lonardo@synclab.it>
2020-09-28 16:24:12 +0200

    caricamento excel prova 2

--- tests/const.py
+++
@@ -1,2 +0,0 @@
-# EXEC = 'coverage run -a'
-EXEC = 'python3'
--- tests2/retrive_data.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python3 ../odoo_export_thread.py -c conf/connection.conf --file=retrieved/sync_employee.doctor.csv --model='sync_employee.doctor' --worker=1 --size=200 --domain="[]" --field="id,department_id_m2m:id" --sep=";" --encoding=utf-8-sig
